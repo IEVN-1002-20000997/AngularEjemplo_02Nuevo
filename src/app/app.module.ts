09422574c7b2501205f1eaefe2cc3dacfec20144
Diego Frausto <frausto.hernandez.diego@gmail.com>
2023-11-04 10:18:21 -0600

    041123 DEFH

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material/material.module';
+import { AgregarComponent } from './utl/agregar/agregar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    AgregarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
